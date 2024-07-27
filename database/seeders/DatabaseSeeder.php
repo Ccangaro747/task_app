8d53d3e6ba9947f26f13f4365deb6912e301b66e
Camilo Cangaro <camilocangaro@gmail.com>
2024-07-27 18:52:08 -0300

    Commit inicial - Añadido relación Usuario-Comentario y migraciones

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Puedo llamar a los seeders que quiera en el orden que quiera
+        $this->call(UsersSeeder::class);
     }
 }
