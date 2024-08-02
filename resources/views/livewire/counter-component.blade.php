f3502ffefc9fbdb98487a30879e46e9d7ce70fc8
Camilo Cangaro <camilocangaro@gmail.com>
2024-08-02 13:15:02 -0300

    feat: Agregar funcionalidad de incremento al componente de prueba contador
    Este commit agrega la funcionalidad de incremento al componente de contador. Se ha añadido una nueva propiedad `$count` al componente `CounterComponent` y se ha creado un método `increment()` que incrementa el valor de `$count` en 1. Además, se ha actualizado la vista `counter-component.blade.php` para mostrar el valor de `$count` y se ha agregado un botón para llamar al método `increment()`.

--- resources/views/livewire/counter-component.blade.php
+++ resources/views/livewire/counter-component.blade.php
@@ -1,3 +1,6 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <p>El contador es: {{ $count }}</p>
+    <button wire:click="increment" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Incrementar
+    </button>
 </div>
